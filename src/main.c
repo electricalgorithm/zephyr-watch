d87a67eea1fbfcfe90641f3ea138691fdd2a64d8
Gyokhan Kochmarla <gokhan.kocmarli@gmail.com>
2024-05-12 14:58:31 +0300

    feat: implement brightness slider callback

--- src/main.c
+++ src/main.c
@@ -15,13 +15,10 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <lvgl_input_device.h>
+#include "user_interface/ui.h"
 
-#define LOG_LEVEL	CONFIG_LOG_DEFAULT_LEVEL
-LOG_MODULE_REGISTER(SmartWatch_OS);
+LOG_MODULE_REGISTER(SmartWatch_OS, LOG_LEVEL_INF);
 
-static void lv_btn_click_callback(lv_event_t *e) {
-	LOG_INF("Button clicked");
-}
 
 int main(void) {
 
@@ -31,20 +28,12 @@ int main(void) {
 		LOG_ERR("Display device is not ready, exiting...");
 		return 0;
 	}
-    LOG_INF("Display device is ready.");
 
     const struct pwm_dt_spec backlight = PWM_DT_SPEC_GET_BY_IDX(DT_NODELABEL(pwm_lcd0), 0);
     if (!pwm_is_ready_dt(&backlight)) {
         LOG_ERR("PWM device is not ready, exiting...");
         return 0;
     }
-    LOG_INF("PWM device is ready.");
-
-    // Log PWM backlight information.
-    LOG_INF("PWM device: %s", backlight.dev->name);
-    LOG_INF("PWM channel: %d", backlight.channel);
-    LOG_INF("PWM period: %d", backlight.period);
-    LOG_INF("PWM flags: %d", backlight.flags);
 
     // Initialize the PWM device.
     int ret = pwm_set_dt(&backlight, 500, 250);
@@ -54,27 +43,13 @@ int main(void) {
     }
 
     // Initialize the display device with initial GUI.
-	if (IS_ENABLED(CONFIG_LV_Z_POINTER_INPUT)) {
-
-        // Create a button.
-		lv_obj_t *hello_world_button = lv_btn_create(lv_scr_act());
-		lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, -15);
-		lv_obj_add_event_cb(hello_world_button,
-                            lv_btn_click_callback,
-                            LV_EVENT_CLICKED, NULL);
-		// Create a label for the button.
-        lv_obj_t *hello_world_label = lv_label_create(hello_world_button);
-        lv_label_set_text(hello_world_label, "Hello world!");
-	    lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
-        
-        LOG_INF("LVGL initialized.");
-	}
+    ui_init();
 
     lv_task_handler();
     display_blanking_off(display_dev);
 
 	while (1) {
 		lv_task_handler();
-		k_sleep(K_MSEC(10));
+		k_usleep(10);
 	}
 }
