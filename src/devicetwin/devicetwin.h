3145459c28709b8568d28c64a3e7561d6dc8b55c
Gyokhan Kochmarla <gokhan.kocmarli@gmail.com>
2025-08-03 00:22:15 +0200

    feat: add notifications to device twin

--- src/devicetwin/devicetwin.h
+++ src/devicetwin/devicetwin.h
@@ -14,6 +14,29 @@
 extern "C" {
 #endif
 
+// Definitions
+#define MAX_NOTIFICATION_COUNT 10
+
+/** NotificationCategory represents the category of a notification. */
+typedef enum {
+    MESSAGE,    // Represents a normal notification. Nothing urgent.
+    ALERT       // Represents an urgent notification requiring immediate attention.
+} NotificationCategory;
+
+/** NotificationItem struct holds the information regarding to a notification recevied.
+ * It is mainly used in receving smartphone notifications in the smartwatch. The system isolates
+ * the local notification storage for efficient processing and retrieval.
+ * This struct implements the items and an array holds them all.
+ */
+typedef struct NotificationItem {
+    uint32_t id;
+    char sender[50];
+    char title[50];
+    char message[200];
+    uint32_t timestamp;
+    NotificationCategory category;
+} NotificationItem;
+
 /* A struct that holds the device settings.
  * This datatype will hold the neccesarry details
  * about the smart-watch's daily usage such as
@@ -22,6 +45,7 @@ extern "C" {
 typedef struct {
     uint32_t unix_time;
     int8_t utc_zone;
+    NotificationItem notifications[MAX_NOTIFICATION_COUNT];
 } device_twin_t;
 
 /*
